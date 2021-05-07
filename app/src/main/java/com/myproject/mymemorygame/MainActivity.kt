0489d02e780f8fc612e1379c234bdece169befae
glezzz <alexglezk@gmail.com>
2021-05-07 23:41:40 +0200

    created main ui

--- app/src/main/java/com/myproject/mymemorygame/MainActivity.kt
+++ app/src/main/java/com/myproject/mymemorygame/MainActivity.kt
@@ -2,10 +2,20 @@ package com.myproject.mymemorygame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.myproject.mymemorygame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val rvBoard by lazy { binding.rvBoard }
+    private val tvNumMoves by lazy { binding.tvNumMoves }
+    private val tvNumPairs by lazy { binding.tvNumPairs }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
